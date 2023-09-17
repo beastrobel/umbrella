6bb79ef99505fbb7d08905e6238b86c263ccc7f7
beastrobel <beatrizstrobel@proton.me>
2023-09-17 16:42:10 -0400

    Successfully displays weather data in imperial units, and rounds temperature to the nearest whole number.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 /* Geolocation instructions from W3 Schools, modified for this project */
 var userLocation = document.getElementById("yourLocation");
 var todaysDate = dayjs().format('MMMM D, YYYY');
+var localWeather = document.getElementById("yourWeather");
+var localTemp = document.getElementById("yourTemp");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -14,14 +16,23 @@ function showPosition(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
     "<br>Longitude: " + lon;
-        var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' +lon + '&appid=0282671f74388449f4d4c1e0b2dbe75e';
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=0282671f74388449f4d4c1e0b2dbe75e&units=imperial';
         fetch(requestUrl)
           .then(function (response) {
             return response.json();
           })
           .then(function (data) {
+
             userLocation.innerHTML = data.name;
             console.log(data);
+                function displayTemp() {
+                localTemp.innerHTML = Math.round(data.main.temp);
+                }
+                displayTemp();
+                function displayWeather() {
+                localWeather.innerHTML = data.weather[0].main + '<br/>Wind: ' + data.wind.speed + '<br/>Humidity: ' + data.main.humidity;
+                }
+                displayWeather();
         });
         
     }
@@ -33,3 +44,5 @@ function displayDate() {
     yourDate.innerHTML = todaysDate;
 }
 document.onload = displayDate();
+
+
